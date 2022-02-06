5afcbfe01a97442f67599140512306af93634a13
Swaminathan Narayanan <snaraya@snaraya-in-ll02.eu.trimblecorp.net>
2022-02-06 15:19:58 +0530

    Added Tests

--- src/test/java/automation/tests/Booking_Services_Tests.java
+++ src/test/java/automation/tests/Booking_Services_Tests.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class) 	
-@CucumberOptions(tags= {" @test"},features = "src/test/resources/APITests.feature",
+@CucumberOptions(features = "src/test/resources/APITests.feature",
 glue = "automation.steps")
 public class Booking_Services_Tests {
 
